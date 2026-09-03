81889d8718102da7597b7fd918e112893d497a66
Turjopro <mdtatrimtafsir@gmail.com>
2026-09-03 10:49:59 +0600

    loading add

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-white">
+    <div  role="status" aria-label="Loading" className="flex h-screen w-full items-center justify-center bg-white">
       <div className="h-14 w-14 animate-spin rounded-full border-4 border-primary border-t-transparent"></div>
     </div>
   );
